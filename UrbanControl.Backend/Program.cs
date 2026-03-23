e173a59c28482aa97571eeac32a10a02ecc84e10
Samuel Seña Uluquipa <senauluquipas@gmail.com>
2026-03-22 20:06:04 -0400

    swagger on root path

--- UrbanControl.Backend/Program.cs
+++ UrbanControl.Backend/Program.cs
@@ -100,7 +100,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UrbanControl API v1");
+    c.RoutePrefix = string.Empty; // Swagger en la raíz "/"
+});
 
 app.UseHttpsRedirection();
 
